84c82600e2427a36759c5c8d7d626ef74e356f0f
whhjdi <418605872@qq.com>
2019-07-14 11:06:11 +0800

    meta title

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,15 @@
 import Vue from "vue";
 import Router from "vue-router";
 import routes from "./routes";
-
+import { setTitle } from "@/utils/util";
 Vue.use(Router);
 
 const router = new Router({
   routes
 });
 
+router.beforeEach((to, from, next) => {
+  to.meta && setTitle(to.meta.title);
+  next();
+});
 export default router;
--- src/router/routes.js
+++ src/router/routes.js
@@ -3,13 +3,19 @@ const routes = [
   {
     path: "/",
     name: "home",
-    component: Home
+    component: Home,
+    meta: {
+      title: "首页"
+    }
   },
   {
     path: "/about",
     name: "about",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    meta: {
+      title: "关于"
+    }
   }
 ];
 
