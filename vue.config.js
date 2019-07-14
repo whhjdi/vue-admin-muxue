0360eae2ee55c053dfcd4cc855ccc46fb69d82a1
whhjdi <418605872@qq.com>
2019-07-14 10:57:08 +0800

    项目初始化和目录结构调整

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const resolve = dir => path.join(__dirname, dir);
+
+const publicPath = process.env.NODE_ENV === "production" ? "/dist" : "/";
+
+module.exports = {
+  publicPath,
+  chainWebpack: config => {
+    config.resolve.alias.set("@", resolve("src"));
+  },
+  productionSourceMap: false,
+  devServer: {
+    proxy: "http://localhost:9527"
+  }
+};
