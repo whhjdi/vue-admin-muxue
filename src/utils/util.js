9d3765fc03df6fb4fd424e605d9d313c0f1f5473
whhjdi <418605872@qq.com>
2019-07-17 20:14:05 +0800

    TreePage

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,3 +1,51 @@
+import clonedeep from "clonedeep";
 export const setTitle = title => {
   window.document.title = title || "vue-admin-muxue";
+};
+
+export const putFileToFolder = (folderList, fileList) => {
+  const folderListCloned = clonedeep(folderList);
+  const fileListCloned = clonedeep(fileList);
+
+  return folderListCloned.map(item => {
+    const folderId = item.id;
+    let index = fileListCloned.length;
+    while (--index >= 0) {
+      const fileItem = fileListCloned[index];
+      if (fileItem.folder_id === folderId) {
+        const file = fileListCloned.splice(index, 1)[0];
+        file.title = file.name;
+        if (item.children) {
+          item.children.push(file);
+        } else {
+          item.children = [file];
+        }
+      }
+    }
+    item.type = "folder";
+    return item;
+  });
+};
+
+export const putFolderToTree = folderList => {
+  if (!folderList.length) return [];
+  const folderListCloned = clonedeep(folderList);
+  const handle = id => {
+    let arr = [];
+    folderListCloned.forEach(item => {
+      if (item.folder_id === id) {
+        const children = handle(item.id);
+        if (item.children) {
+          item.children = [].concat(item.children, children);
+        } else {
+          item.children = children;
+        }
+        item.title = item.name;
+        arr.push(item);
+      }
+    });
+    return arr;
+  };
+
+  return handle(0);
 };
--- src/views/TreePage.vue
+++ src/views/TreePage.vue
@@ -1,22 +1,57 @@
 <template>
-  <div>1</div>
+  <div class="my-tree-wrapper">
+    <Tree :data="treeList" :render="renderContent"></Tree>
+  </div>
 </template>
 
 <script>
 import { getFileList, getFolderList } from "@/api/data.js";
-
+import { putFileToFolder, putFolderToTree } from "@/utils/util";
 export default {
   name: "TreePage",
+  data() {
+    return {
+      folderList: [],
+      fileList: [],
+      treeList: [],
+      renderContent(h, { root, node, data }) {
+        console.log(1, data, root, node);
+        return (
+          <div class="my-folder-wrapper">
+            {data.type === "folder" ? (
+              <Icon type="ios-folder" class="my-icon" />
+            ) : (
+              <Icon type="logo-apple" class="my-icon" />
+            )}
 
+            {data.title}
+          </div>
+        );
+      }
+    };
+  },
+  methods: {},
   mounted() {
-    getFolderList().then(res => {
-      console.log(res.data);
-    });
-    getFileList().then(res => {
-      console.log(res.data);
+    Promise.all([getFolderList(), getFileList()]).then(res => {
+      this.folderList = res[0].data;
+      this.fileList = res[1].data;
+      const arr = putFileToFolder(this.folderList, this.fileList);
+      const arr2 = putFolderToTree(arr);
+      this.treeList = arr2;
     });
   }
 };
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less">
+.my-tree-wrapper {
+  padding: 18px;
+  background: #fff;
+  .my-folder-wrapper {
+    display: inline-block;
+    .my-icon {
+      margin-right: 5px;
+    }
+  }
+}
+</style>
