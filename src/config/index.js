6200a57658b5611465bcd3261df059136406b6f2
muxue <418605872@qq.com>
2020-05-10 21:07:44 +0800

    remove easymock

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,8 @@
+// export const baseURL =
+//   process.env.NODE_ENV === "production"
+//     ? "https://www.easy-mock.com/mock/5cc1a05e744f861c0e22a4c0"
+//     : "http://localhost:9527";
 export const baseURL =
   process.env.NODE_ENV === "production"
-    ? "https://www.easy-mock.com/mock/5cc1a05e744f861c0e22a4c0"
+    ? "https://whhjdi.github.io/vue-admin-muxue"
     : "http://localhost:9527";
